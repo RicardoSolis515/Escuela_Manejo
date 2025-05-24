e790aa3e2cd2618b912f305ee382e56375059375
Ricardo Solis <rsolis@tecjerez.edu.mx>
2025-05-24 16:22:23 -0600

    Internalframe para añadir lecciones creado

--- src/Ventanas/VentanaInicio.java
+++ src/Ventanas/VentanaInicio.java
@@ -7,9 +7,11 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyVetoException;
 
 public class VentanaInicio extends JFrame implements ActionListener, Runnable {
 
+    //----------------------------------ELEMENTOS PARA MENUBAR----------------------------------------------
     Menu menuLecciones, menuAdministrativos, menuInstructores, menuClientes, menuVehiculos, menuEntrevistas;
     MenuItem itemAñadirLec, itemAdministrarLec,
             itemAñadirAdm, itemAdministrarAdm,
@@ -18,6 +20,23 @@ public class VentanaInicio extends JFrame implements ActionListener, Runnable {
             itemAñadirVeh, itemAdministrarVeh,
             itemAñadirEnt, itemAdministrarEnt;
 
+
+
+    //----------------------------------ELEMENTOS PARA IFAÑADIR-------------------------------------------
+    JInternalFrame IFAñadorLec;
+    JComboBox cajaInstructor, cajaCliente, cajaMes, cajaDia, cajaAño, cajahora;
+    JRadioButton rbCompuesta;
+
+    JButton btnAñadir, btnBorrar, btnCancelar;
+
+
+
+    //----------------------------------ELEMENTOS PARA IFADMINISTRAR------------------------------------
+
+
+
+
+    //---------------------------------VENTANAINICIO------------------------------------
     public VentanaInicio() {
         getContentPane().setLayout(null);
 
@@ -39,6 +58,8 @@ public class VentanaInicio extends JFrame implements ActionListener, Runnable {
                 itemAdministrarLec = new MenuItem("Administrar");
                 menuLecciones.add(itemAñadirLec);
                 menuLecciones.add(itemAdministrarLec);
+                itemAñadirLec.addActionListener(this);
+                itemAdministrarLec.addActionListener(this);
 
             menuInicio.add(menuLecciones);
 
@@ -90,11 +111,92 @@ public class VentanaInicio extends JFrame implements ActionListener, Runnable {
 
         setMenuBar(menuInicio);
 
+        IFAñadorLec = new JInternalFrame();
+        IFAñadorLec.setSize(400,400);
+        IFAñadorLec.setVisible(false);
+        IFAñadorLec.setClosable(false);
+        IFAñadorLec.setMaximizable(false);
+        IFAñadorLec.setIconifiable(false);
+        IFAñadorLec.setResizable(false);
+        IFAñadorLec.setDefaultCloseOperation(HIDE_ON_CLOSE);
+        IFAñadorLec.getContentPane().setLayout(null);
+        try {
+            IFAñadorLec.setMaximum(true);
+        } catch (PropertyVetoException e) {
+
+        }
+
+
+            cajaInstructor = new JComboBox();
+            cajaInstructor.setBounds(20, 20, 150, 25);
+            cajaInstructor.addActionListener(this);
+            IFAñadorLec.add(cajaInstructor);
+
+            cajaCliente = new JComboBox();
+            cajaCliente.setBounds(200, 20, 150, 25);
+            cajaCliente.addActionListener(this);
+            IFAñadorLec.add(cajaCliente);
+
+            cajaMes = new JComboBox();
+            cajaMes.setBounds(20, 60, 100, 25);
+            cajaMes.addActionListener(this);
+            IFAñadorLec.add(cajaMes);
+
+            cajaDia = new JComboBox();
+            cajaDia.setBounds(130, 60, 50, 25);
+            cajaDia.addActionListener(this);
+            IFAñadorLec.add(cajaDia);
+
+            cajaAño = new JComboBox();
+            cajaAño.setBounds(190, 60, 70, 25);
+            cajaAño.addActionListener(this);
+            IFAñadorLec.add(cajaAño);
+
+            cajahora = new JComboBox();
+            cajahora.setBounds(270, 60, 80, 25);
+            cajahora.addActionListener(this);
+            IFAñadorLec.add(cajahora);
+
+            rbCompuesta = new JRadioButton("Clase compuesta");
+            rbCompuesta.setBounds(20, 100, 200, 25);
+            rbCompuesta.addActionListener(this);
+            IFAñadorLec.add(rbCompuesta);
+
+            btnAñadir = new JButton("Añadir");
+            btnAñadir.setBounds(20, 150, 100, 30);
+            btnAñadir.addActionListener(this);
+            IFAñadorLec.add(btnAñadir);
+
+            btnBorrar = new JButton("Borrar");
+            btnBorrar.setBounds(140, 150, 100, 30);
+            btnBorrar.addActionListener(this);
+            IFAñadorLec.add(btnBorrar);
+
+            btnCancelar = new JButton("Cancelar");
+            btnCancelar.setBounds(260, 150, 100, 30);
+            btnCancelar.addActionListener(this);
+            IFAñadorLec.add(btnCancelar);
+
+        add(IFAñadorLec);
+
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource() instanceof MenuItem){
+
+            if(e.getSource()==itemAñadirLec){
+                System.out.println("AÑADIR");
+                IFAñadorLec.setVisible(true);
+            }
+            if(e.getSource()==itemAdministrarLec){
+                System.out.println("ADMINISTRAR");
+            }
+
+        }else{
+
+        }
 
     }
 
